32ff26143525a3a6e287e66805a7579c6c510a23
James McManus <jjm439@cornell.edu>
2018-03-19 20:49:42 -0400

    added groups names to search_controller

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -3,7 +3,7 @@ from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
 project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+net_id = "James McManus: jjm439, Kristian Langholm: krl38, Faadhil Moheed: fm363, Darien Lin: dl724, Jatin Bharwani: jsb399"
 
 @irsystem.route('/', methods=['GET'])
 def search():

a2f009471a6a9ace407861880e5346a4752f7394
Kristian Langholm <>
2018-04-09 17:07:38 -0400

    Added an Exercise module with a dummy get_exercises function. Will fill it out soon

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,6 +1,7 @@
 from . import *  
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+from app.irsystem.models.exercise import Exercise
 
 project_name = "Ilan's Cool Project Template"
 net_id = "James McManus: jjm439, Kristian Langholm: krl38, Faadhil Moheed: fm363, Darien Lin: dl724, Jatin Bharwani: jsb399"
@@ -13,7 +14,7 @@ def search():
 		output_message = ''
 	else:
 		output_message = "Your search: " + query
-		data = range(5)
+		data = Exercise.get_exercises(name = query)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 

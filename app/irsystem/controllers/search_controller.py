73a6fcc672c8d14b0645f7fff6e6d6c7d1279cab
James McManus <jjm439@cornell.edu>
2018-04-11 11:53:08 -0400

    test

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,9 +1,9 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 from app.irsystem.models.exercise import Exercise
 
-project_name = "Exercise Planner"
+project_name = "Ilan's Cool Project Template"
 net_id = "James McManus: jjm439, Kristian Langholm: krl38, Faadhil Moheed: fm363, Darien Lin: dl724, Jatin Bharwani: jsb399"
 
 @irsystem.route('/', methods=['GET'])
@@ -14,7 +14,7 @@ def search():
 		output_message = ''
 	else:
 		output_message = "Your search: " + query
-		data = Exercise.get_exercises(name = query)
+		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 

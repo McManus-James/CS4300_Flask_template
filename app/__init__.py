70d63a50a7610cab84d933b34ae7cad8cac107b2
JATIN BHARWANI <JATINBHARWANI@GMAIL.COM>
2018-04-13 16:32:39 -0400

    simple search return names

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ monkey.patch_all()
 
 # Imports
 import os
+import pickle
+import json
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
@@ -25,6 +27,47 @@ app.register_blueprint(irsystem)
 
 # Initialize app w/SocketIO
 socketio.init_app(app)
+
+# Load pickle files
+f = open('extra/pickles/description_tfidf','rb')
+desc_tfidf = pickle.load(f)
+app.config['desc_tfidf'] = desc_tfidf
+
+f = open('extra/pickles/equipment_tfidf','rb')
+equip_tfidf = pickle.load(f)
+app.config['equip_tfidf'] = equip_tfidf
+
+f = open('extra/pickles/muscles_tfidf','rb')
+muscles_tfidf = pickle.load(f)
+app.config['muscles_tfidf'] = muscles_tfidf
+
+f = open('extra/pickles/name_tfidf','rb')
+name_tfidf = pickle.load(f)
+app.config['name_tfidf'] = name_tfidf
+
+f = open('extra/pickles/description_vocab_to_index','rb')
+description_vocab_to_index = pickle.load(f)
+app.config['description_vocab_to_index'] = description_vocab_to_index
+
+f = open('extra/pickles/equipment_vocab_to_index','rb')
+equipment_vocab_to_index = pickle.load(f)
+app.config['equipment_vocab_to_index'] = equipment_vocab_to_index
+
+f = open('extra/pickles/muscles_vocab_to_index','rb')
+muscles_vocab_to_index = pickle.load(f)
+app.config['muscles_vocab_to_index'] = muscles_vocab_to_index
+
+f = open('extra/pickles/name_vocab_to_index','rb')
+name_vocab_to_index = pickle.load(f)
+app.config['name_vocab_to_index'] = name_vocab_to_index
+
+f = open('extra/pickles/vector_index_to_exercise','rb')
+vector_index_to_exercise = pickle.load(f)
+app.config['vector_index_to_exercise'] = vector_index_to_exercise
+
+f = open('extra/jefit/data.json','rb')
+raw_data = json.load(f)
+app.config['raw_data'] = raw_data
 
 # HTTP error handling
 @app.errorhandler(404)
--- app/irsystem/models/exercise.py
+++ app/irsystem/models/exercise.py
@@ -1,3 +1,11 @@
+import pickle
+import numpy as np
+from numpy import linalg as LA
+import os
+import nltk
+from flask import current_app as app
+
+
 class Exercise:
 
 	@classmethod
@@ -7,4 +15,55 @@ class Exercise:
 	#returns exercises which match each criteria (intersection). Routine 
 	#determines whether to return individual exercises or a routine
 	def get_exercises(self, name = None, muscles = None, equipment = None, routine = None):
-		return name.split()
+		if True:			#Place Holder for simple search check 
+			return self.simple_search(name)
+		else:
+			return advance_search(name, muscles, equipment, routine)
+
+	@classmethod
+	def simple_search(self, query, desc_w=.25, equip_w=.25, musc_w=.25, name_w=.25):
+		# print app.config['desc_tfidf'].shape
+		# print app.config['equip_tfidf'].shape
+		# print app.config['muscles_tfidf'].shape
+		# print app.config['name_tfidf'].shape
+		# print len(app.config['description_vocab_to_index'])
+		# print len(app.config['equipment_vocab_to_index'])
+		# print len(app.config['muscles_vocab_to_index'])
+		# print len(app.config['name_vocab_to_index'])
+		# print len(app.config['vector_index_to_exercise'])
+		dics = [app.config['description_vocab_to_index'], app.config['equipment_vocab_to_index'], app.config['muscles_vocab_to_index'], app.config['name_vocab_to_index']]
+		tf_idfs = [app.config['desc_tfidf'], app.config['equip_tfidf'], app.config['muscles_tfidf'], app.config['name_tfidf']]
+		query_tokens = nltk.word_tokenize(query)
+		query_vecs = []
+		for dic in dics:
+			q_vec = np.zeros((len(dic),1))
+			for t in query_tokens:
+				if t in dic: 
+					q_vec[dic[t]] += 1.0
+			query_vecs.append(q_vec)
+		
+		for i in range(len(tf_idfs)):
+			cpy = np.copy(tf_idfs[i])
+			cpy[cpy > 0.0] = 1
+			df = np.sum(cpy, axis=0)
+			df.reshape(df.shape[0] , 1)
+			query_vecs[i] = np.multiply(query_vecs[i].T, np.log(1299 / (1+df)))
+			# print query_vecs[i].shape
+
+		if all([np.count_nonzero(x) == 0 for x in query_vecs]):
+			return "No_Valid_Query_Terms".split()
+
+		sim_matrices = []
+		for i in range(len(tf_idfs)):
+			sim_matrices.append(np.dot(tf_idfs[i], query_vecs[i].T)) 
+
+		weighted_sim = (desc_w*sim_matrices[0] + equip_w*sim_matrices[1] + musc_w*sim_matrices[2] + name_w*sim_matrices[3])
+		sorted_ind = np.argsort(weighted_sim, axis=0)[::-1]
+
+		result = []
+		for i in range(10):	
+			index = app.config['vector_index_to_exercise'][int(sorted_ind[i])]
+			entry = app.config['raw_data'][index]
+			result.append(entry['name'])
+
+		return result

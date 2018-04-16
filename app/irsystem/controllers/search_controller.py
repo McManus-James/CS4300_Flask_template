9c5480383b3703afdddef7028bd85282014aedc1
fm363 <fm363@cornell.edu>
2018-04-15 23:18:55 -0400

    test

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -6,7 +6,7 @@ from app.irsystem.models.exercise import Exercise
 project_name = "Exercise Planner"
 net_id = "James McManus: jjm439, Kristian Langholm: krl38, Faadhil Moheed: fm363, Darien Lin: dl724, Jatin Bharwani: jsb399"
 
-@irsystem.route('/', methods=['GET'])
+@irsystem.route('/search', methods=['GET'])
 def search():
 	query = request.args.get('search')
 	if not query:
